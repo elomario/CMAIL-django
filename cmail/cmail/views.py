894aa90a7e867f515b06b959ffc62f1cec409d16
AlTremor <alex.triquet@insa-lyon.fr>
2015-05-28 15:01:44 +0200

    modifié :         urls.py 	modifié :         views.py

--- cmail/cmail/views.py
+++ cmail/cmail/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 # Create your views here.
 
-def home(request):
-	return render(request,'home.html')
+
+def homev(request):
+	loggedin=True
+	if(loggedin!=True):
+		return loginv(request)
+	else:
+		return HttpResponse("<html><body>YOU ARE HOME</body></html>")
+def loginv(request):
+	loggedin=False
+	if(loggedin==True):
+		return(homev(request))
+	return HttpResponse("<html><body>YOU ARE NOT LOGGED IN</body></html>")
+def accountv(request):
+	loggedin=False
+	if(loggedin!=True):
+		return(homev(request))
+	return HttpResponse("<html><body>YOU ARE ON YOUR ACCOUNT SETTINGS</body></html>")
+
