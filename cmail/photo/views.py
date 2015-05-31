9ff172ca37bd1a2534b32aeee7fdd6b099829f76
Omar Mater <omarmater@yahoo.fr>
2015-05-31 14:26:22 +0200

    Image Upload
    Working on image upload, fonction & html

--- cmail/photo/views.py
+++ cmail/photo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from photo import Photo
+def upload_photo(request):
+    if request.method == 'POST':
+        uploaded_photo = Photo(image = request.POST)
+        if uploaded_photo.is_valid():
+            # file is saved
+            uploaded_photo.save()
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = ModelFormWithFileField()
+    return render(request, 'upload.html', {'form': form})
 # Create your views here.
