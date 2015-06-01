0b1322a43a5a11f3dd2d9b52ec4bafb44aa3cedb
Omar Mater <omarmater@yahoo.fr>
2015-06-01 11:33:37 +0200

    decommenter admin photo

--- cmail/photo/admin.py
+++ cmail/photo/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from photo.models import Photo
 
-#class InlineImage(admin.TabularInline):
-	#model = image
+class InlineImage(admin.TabularInline):
+	model = image
 
 class PhotoAdmin(admin.ModelAdmin):
 	list_display = ('publication_date','id','phototype','image')
-	#inlines=[InlineImage]
+	inlines=[InlineImage]
 	
 admin.site.register(Photo,PhotoAdmin)
