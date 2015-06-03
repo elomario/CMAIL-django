376c9d634167dbafa26a2b699f5521a3cd186565
AlTremor <alex.triquet@insa-lyon.fr>
2015-06-03 11:02:24 +0200

    modifié :         cmail/views.py 	modifié :         t/box.html

--- cmail/cmail/views.py
+++ cmail/cmail/views.py
@@ -6,7 +6,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Template, Context, RequestContext
 from django.template.loader import get_template
 from box.models import Box
-from member.models import Member 
+from member.models import Member
+from sim.models import Sim 
 # Create your views here.
 
 
@@ -28,20 +29,54 @@ def boxv(request):
 	v='48'
 	usera=request.user
 	box_list=[]
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect("/login/")
-        else:
-		myusername=User.objects.get(username=usera)
-		try:
-			mymember=Member.objects.get(member=myusername)
-		except Member.DoesNotExist:
-			mymember=None
-		for boxu in Box.objects.filter(member=mymember):
-    			box_list.append(boxu)
-		t=get_template('box.html')
-        	html = t.render(RequestContext(request, {'vor':v,'username':usera,'mybox_list':box_list}))
-        	return HttpResponse(html)
+	myusername=User.objects.get(username=usera)
+	try:
+		mymember=Member.objects.get(member=myusername)
+        except Member.DoesNotExist:
+              	mymember=None
 
+	if request.method =='POST':
+		new_simn=request.POST.get('simn','')
+		new_sim=Sim(number=new_simn, description='')
+		new_sim.save()
+		#mymember=
+		return HttpResponseRedirect("/box/")
+	else:
+		if not request.user.is_authenticated():
+			return HttpResponseRedirect("/login/")
+        	else:
+			for boxu in Box.objects.filter(member=mymember):
+    				box_list.append(boxu)
+			t=get_template('box.html')
+        		html = t.render(RequestContext(request, {'vor':v,'username':usera,'mybox_list':box_list}))
+        		return HttpResponse(html)
+	
+def boxvb(request):
+	v='48'
+	usera=request.user
+	box_list=[]
+	if not request.user.is_authenticated():
+			return HttpResponseRedirect("/login/")
+        else:	
+		myusername=User.objects.get(username=usera)
+		mymember=Member.objects.get(member=myusername)
+		if request.method =='POST':
+			new_simn=request.POST.get('simn','')
+			new_sim=Sim(number=new_simn, description='')
+			new_sim.save()
+			new_box=Box(sim=new_sim,address='')
+			new_box.save()
+			new_box.member.add(mymember)
+			return HttpResponseRedirect("/box/")
+		else:
+			if not request.user.is_authenticated():
+				return HttpResponseRedirect("/login/")
+        		else:
+				for boxu in Box.objects.filter(member=mymember):
+    					box_list.append(boxu)
+				t=get_template('box.html')
+        			html = t.render(RequestContext(request, {'vor':v,'username':usera,'mybox_list':box_list}))
+        			return HttpResponse(html)		
 def loginv(request):
 	v= '44'
 	if request.method == 'GET':	
@@ -74,6 +109,8 @@ def registerv(request):
         	form = UserCreationForm(request.POST)
         	if form.is_valid():
             		new_user = form.save()
+			new_member=Member(member=new_user,name='Blank',surname='Blank',billingaddress='Blank')
+			new_member.save()
             		return HttpResponseRedirect("/account/")
 	else:
 		request.user.is_authenticated=False
