d18d8912ec67153e93b7238c0243fb386ae0e17b
Omar Mater <omarmater@yahoo.fr>
2015-06-03 11:01:28 +0200

    photo upload and notification send
    pouet

--- cmail/notification/views.py
+++ cmail/notification/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def send_notification(request):
+	if request.method == 'GET':
+		for last_photo in Photo.all()
+			myPhoto=last_photo
+		#integrate matlab exec for image handling
+		myPhoto.type="colis"
