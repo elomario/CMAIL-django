9b34158c0b15a95b1afee4f638a7d566036af03c
Omar Mater <omarmater@yahoo.fr>
2015-06-04 08:44:09 +0200

    foreign key photo in notification model

--- cmail/notification/models.py
+++ cmail/notification/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from box.models import Box
+
+from photo.models import Photo
 # Create your models here.
 
 class Notification(models.Model):
 	title=models.CharField(max_length = 45, blank = True)
 	description = models.CharField(max_length = 45, blank = True)
 	box=models.ForeignKey(Box)
+	photo = models.ForeignKey(Photo, null=True)
 	def __str__(self):  
 			return self.title
--- cmail/photo/models.py
+++ cmail/photo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from cmail import settings
-from notification.models import Notification
 # Create your models here.
 
 class Photo(models.Model):
@@ -14,7 +13,9 @@ class Photo(models.Model):
 	image = models.ImageField(upload_to = 'static', blank=True)
 	phototype = models.CharField(max_length = 9,choices = TYPE_CHOICES, blank = True)
 	publication_date = models.DateTimeField('published on:', auto_now_add = True)
-	notification = models.ForeignKey(Notification, null=True)
+	
+	
+	
 	def image_tag(self):
 		return u'<img src="/%s" width="100" height="100" />' % self.image.url
 	
