68118112cf84b0cc3987d52b2b5677d7efad671c
AlTremor <alex.triquet@insa-lyon.fr>
2015-06-16 23:10:11 +0200

    modifié :         cmail/views.py 	modifié :         member/models.py 	modifié :         t/account.html

--- cmail/member/models.py
+++ cmail/member/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here.
 
 class Member(models.Model):
@@ -7,5 +9,6 @@ class Member(models.Model):
 	name = models.CharField(max_length = 20, blank = True)
 	surname = models.CharField(max_length = 20, blank = True)
 	billingaddress =  models.CharField(max_length = 45, blank = True)
+	phone= PhoneNumberField(blank=True)
 	def __str__(self):  
 		return self.member.username
